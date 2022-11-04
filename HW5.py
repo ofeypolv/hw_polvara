c2bf53de43c10bf02cdea89bc821a0e10804112a
ofeypolv <camipolv@gmail.com>
2022-11-03 21:21:45 -0400

    HW5 upload

--- HW5.py
+++ HW5.py
@@ -4,6 +4,10 @@ import matplotlib.pyplot as plt
 import requests
 import argparse
 
+# ARGUMENTS NEED TO BE ENTERED SEPARATED BY SPACE, dow.txt/dow2.txt 10%/2% CFT/CDT. 
+# For example if I want to use dow.txt, keeping the first 10% of Fourier coefficients and using CFT expansion,
+# I would write >>python HW5.py dow.txt 10% CFT
+
 #1D discrete cosine transform
 def dct(y):
     N = len(y)
@@ -35,10 +39,20 @@ percs = {"10%": 0.1,"2%": 0.02}
 fts = {"DFT": [rfft,irfft],"DCT": [dct,idct]}
 
 parser = argparse.ArgumentParser(description="Dow Jones closing values")
-parser.add_argument("URL", type=str,help='Dow Jones closing values file')
-parser.add_argument("perc", type=str,help='Percentage of Fourier coefficients to consider')
-parser.add_argument("ft", type=str,help='Type of Fourier transform to use')
+ref_arg1 = parser.add_argument("URL", type=str,help='Dow Jones closing values file')
+ref_arg2 = parser.add_argument("perc", type=str,help='Percentage of Fourier coefficients to consider')
+ref_arg3 = parser.add_argument("ft", type=str,help='Type of Fourier transform to use')
 args = parser.parse_args()
+
+'''# raising error in case of wrong args
+if (args.URL != "dow.txt") or (args.URL != "dow2.txt"):
+    raise argparse.ArgumentError(ref_arg1, "please choose between dow.txt and dow2.txt")
+  
+if (args.perc != "10%") or (args.perc != "2%"):
+    raise argparse.ArgumentError(ref_arg2, f'please choose between {2}% and {10}%')
+
+if (args.ft != "DFT") or (args.ft != "DCT"):
+    raise argparse.ArgumentError(ref_arg2, "please choose between DFT and DCT")'''
 
 URL_run = urls.get(args.URL, "")
 perc_run = percs.get(args.perc, "")
